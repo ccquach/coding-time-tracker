cf460d90cf6e7696461a2d07d826ddb4d21d21fb
cynthia.quach <ccquach93@gmail.com>
2018-10-05 17:50:24 -0700

    enable passport strategy proxy option

--- services/passport.js
+++ services/passport.js
@@ -18,6 +18,7 @@ passport.use(
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
       callbackURL: '/api/auth/google/callback',
+      proxy: true,
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
